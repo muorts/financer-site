b5dc931766abf81f7f67671e851ce7242256f396
Murilo Ortega <muortegapereira.07@gmail.com>
2026-08-03 15:42:51 -0300

    criacao da estrutura de pastas e selecionamento das linguagens para o projeto

---
+++ financer-backend/src/main/java/com/murilo/backend_financer/BackendFinancerApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.backend_financer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFinancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFinancerApplication.class, args);
+	}
+
+}
---
+++ financer-backend/src/test/java/com/murilo/backend_financer/BackendFinancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.backend_financer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFinancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
